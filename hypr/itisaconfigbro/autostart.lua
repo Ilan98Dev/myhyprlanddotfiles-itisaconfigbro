b08d09f76e7685c90e3ef6922ae8d0d0b3006c99
Ilan98Dev <ilhanzafar85@gmail.com>
2026-08-18 19:50:41 +0500

    sm things happening

--- hypr/itisaconfigbro/autostart.lua
+++ hypr/itisaconfigbro/autostart.lua
@@ -8,6 +8,8 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function()
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("nm-applet")
 	hl.exec_cmd("hypridle")
